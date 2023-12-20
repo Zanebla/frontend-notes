a4aecbdf61966a86e02318c12a8a772962fa76b1
Zane <3277696440@qq.com>
2023-12-20 19:32:52 +0800

    feat: type annotation and functions

---
+++ TypeScript/Qf/02-everyday-types/dist/05-function.js
@@ -0,0 +1,15 @@
+"use strict";
+function greet(name) {
+    console.log('hello, ' + name.toUpperCase() + '!');
+}
+greet('Zane');
+function getFavoriteNumber() {
+    return 7;
+}
+const names = ['Zane', 'Tht', 'Lh'];
+names.forEach(function (s) {
+    console.log(s.toUpperCase);
+});
+names.forEach((s) => {
+    console.log(s.toLowerCase);
+});
---
+++ TypeScript/Qf/02-everyday-types/src/05-function.ts
@@ -0,0 +1,18 @@
+function greet(name: string) {
+  console.log('hello, ' + name.toUpperCase() + '!')
+}
+
+greet('Zane')
+
+function getFavoriteNumber(): number {
+  return 7
+}
+
+const names = ['Zane', 'Tht', 'Lh']
+names.forEach(function (s) {
+  console.log(s.toUpperCase)
+})
+
+names.forEach((s) => {
+  console.log(s.toLowerCase)
+})
